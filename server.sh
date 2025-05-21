eef1438a7875d2417521fdfd5b8154c763ba32bf
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-21 18:22:55 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@ git pull
 docker rmi -f $(docker images -aq)
 docker rm -f $(docker ps -a -q)
 docker pull craigdsilva/hotel-server:$(git rev-parse HEAD)
-docker run -d -p 3001:3001 craigdsilva/hotel-server
+docker run -d -p 3001:3001 craigdsilva/hotel-server:$(git rev-parse HEAD)
